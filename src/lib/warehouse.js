c5458aa5b190e1f5b6ac3cd729f9350b77646f41
epitchi <thienvanlea1@gmail.com>
2023-06-12 10:59:49 +0700

    update env key

--- src/lib/warehouse.js
+++ src/lib/warehouse.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.WAREHOUSE_URL;
-const supabaseKey = import.meta.env.WAREHOUSE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_WAREHOUSE_URL;
+const supabaseKey = import.meta.env.VITE_WAREHOUSE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function WarehousePush(event_name, user, data) {
