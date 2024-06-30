5663fbc30578b87e5bb02fc3feb3f5201339491b
carosaome <sieunhankiet@gmail.com>
2024-06-30 13:50:24 +0700

    update store

--- src/containers/applications/apps/store.jsx
+++ src/containers/applications/apps/store.jsx
@@ -143,7 +143,7 @@ const reviews = 5000;
 const DetailPage = ({ app }) => {
     const [dstate, setDown] = useState(0);
 
-    const t = (e) => {};
+    const t = (e) => { };
     const [Options, SetOptions] = useState([]);
     const user = useAppSelector((state) => state.user);
     const stat = useAppSelector((state) => state.user.stat);
@@ -174,6 +174,18 @@ const DetailPage = ({ app }) => {
             appDispatch(popup_open({ type: 'warning', data: {} }));
             return;
         }
+
+        appDispatch(
+            popup_open({
+                type: 'complete',
+                data: {
+                    content: 'Tính năng đang bảo trì, tạm thời chưa thể sử dụng',
+                    success: false
+                }
+            })
+        );
+
+        return
         appDispatch(
             popup_open({
                 type: 'notify',
@@ -345,7 +357,7 @@ const DownPage = ({ action }) => {
     const games = useAppSelector((state) => state.globals.games);
     const [searchtxt, setShText] = useState('');
 
-    const t = (e) => {};
+    const t = (e) => { };
     const [storeApps, setStoreApps] = useState([...apps, ...games]);
     const handleSearchChange = (e) => {
         setShText(e.target.value);
@@ -467,20 +479,20 @@ const DownPage = ({ action }) => {
                 {storeApps.length > 0
                     ? renderSearchResult()
                     : listDraftApp.map((i) => (
-                          <div
-                              key={i}
-                              className="animate-pulse ribcont p-4 pt-8 ltShad prtclk"
-                              data-action="page2"
-                          >
-                              <Image
-                                  className="mx-4 mb-6 rounded bg-slate-200"
-                                  w={100}
-                                  h={100}
-                                  ext
-                              />
-                              <div className="capitalize text-xs text-center font-semibold"></div>
-                          </div>
-                      ))}
+                        <div
+                            key={i}
+                            className="animate-pulse ribcont p-4 pt-8 ltShad prtclk"
+                            data-action="page2"
+                        >
+                            <Image
+                                className="mx-4 mb-6 rounded bg-slate-200"
+                                w={100}
+                                h={100}
+                                ext
+                            />
+                            <div className="capitalize text-xs text-center font-semibold"></div>
+                        </div>
+                    ))}
             </div>
         </div>
     );
