97c873b58f69abad25da50a91932d0f63a75a372
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-09-08 11:08:05 +0000

    format code

--- src/components/start/index.jsx
+++ src/components/start/index.jsx
@@ -199,7 +199,13 @@ export const SidePane = () => {
             >
                 <div className="mainContent">
                     <div className="quickSettings ">
-                        {isMobile ? <MobileComponent pnstates={pnstates}></MobileComponent> : <DesktopComponent pnstates={pnstates} />}
+                        {isMobile ? (
+                            <MobileComponent
+                                pnstates={pnstates}
+                            ></MobileComponent>
+                        ) : (
+                            <DesktopComponent pnstates={pnstates} />
+                        )}
 
                         <div className="sliderCont flex flex-col items-start">
                             <div className="containerSlider">
@@ -217,7 +223,7 @@ export const SidePane = () => {
                                                 100) *
                                                 remote.bitrate +
                                                 MIN_BITRATE()) /
-                                            1000
+                                                1000
                                         )}
                                     </span>
                                 </div>
@@ -243,8 +249,8 @@ export const SidePane = () => {
                                         {Math.round(
                                             ((MAX_FRAMERATE - MIN_FRAMERATE) /
                                                 100) *
-                                            remote.framerate +
-                                            MIN_FRAMERATE
+                                                remote.framerate +
+                                                MIN_FRAMERATE
                                         )}
                                     </span>
                                 </div>
@@ -413,9 +419,7 @@ export const LogMaintain = () => {
     );
 };
 
-
 function MobileComponent({ pnstates }) {
-
     const sidepane = useAppSelector((state) => state.sidepane);
     const t = useAppSelector((state) => state.globals.translation);
 
@@ -431,33 +435,22 @@ function MobileComponent({ pnstates }) {
                             className="qkbtn handcr prtclk"
                             onClick={clickDispatch}
                             data-action={qk.action}
-                            data-payload={
-                                qk.payload || qk.state
-                            }
+                            data-payload={qk.payload || qk.state}
                             data-state={pnstates[idx]}
                         >
-                            {Object.keys(md).includes(
-                                qk.src
-                            ) ? (
+                            {Object.keys(md).includes(qk.src) ? (
                                 (() => {
-                                    const WinApp =
-                                        md[qk.src];
+                                    const WinApp = md[qk.src];
                                     return <WinApp />;
                                 })()
-                            ) : Object.keys(
-                                fi
-                            ).includes(qk.src) ? (
+                            ) : Object.keys(fi).includes(qk.src) ? (
                                 (() => {
-                                    const WinApp =
-                                        fi[qk.src];
+                                    const WinApp = fi[qk.src];
                                     return <WinApp />;
                                 })()
-                            ) : Object.keys(
-                                fa
-                            ).includes(qk.src) ? (
+                            ) : Object.keys(fa).includes(qk.src) ? (
                                 (() => {
-                                    const WinApp =
-                                        fa[qk.src];
+                                    const WinApp = fa[qk.src];
                                     return <WinApp />;
                                 })()
                             ) : (
@@ -466,17 +459,11 @@ function MobileComponent({ pnstates }) {
                                     ui={qk.ui}
                                     src={qk.src}
                                     width={14}
-                                    invert={
-                                        pnstates[idx]
-                                            ? true
-                                            : null
-                                    }
+                                    invert={pnstates[idx] ? true : null}
                                 />
                             )}
                         </div>
-                        <div className="qktext">
-                            {t[qk.name]}
-                        </div>
+                        <div className="qktext">{t[qk.name]}</div>
                     </div>
                 ))}
             </div>
@@ -484,20 +471,13 @@ function MobileComponent({ pnstates }) {
                 <hr className="mb-4" />
                 <div className="listBtn">
                     {sidepane.mobileControl.shortcuts.map((qk, idx) => (
-                        <div
-                            key={idx}
-                            className="qkGrp t"
-                        >
+                        <div key={idx} className="qkGrp t">
                             <div
                                 style={{
                                     fontSize: '0.6rem'
                                 }}
                                 className="qkbtn handcr prtclk"
-                                onClick={() =>
-                                    Actions.clickShortCut(
-                                        qk.val
-                                    )
-                                }
+                                onClick={() => Actions.clickShortCut(qk.val)}
                             >
                                 {qk.name}
                             </div>
@@ -525,33 +505,22 @@ function DesktopComponent({ pnstates }) {
                             className="qkbtn handcr prtclk"
                             onClick={clickDispatch}
                             data-action={qk.action}
-                            data-payload={
-                                qk.payload || qk.state
-                            }
+                            data-payload={qk.payload || qk.state}
                             data-state={pnstates[idx]}
                         >
-                            {Object.keys(md).includes(
-                                qk.src
-                            ) ? (
+                            {Object.keys(md).includes(qk.src) ? (
                                 (() => {
-                                    const WinApp =
-                                        md[qk.src];
+                                    const WinApp = md[qk.src];
                                     return <WinApp />;
                                 })()
-                            ) : Object.keys(fi).includes(
-                                qk.src
-                            ) ? (
+                            ) : Object.keys(fi).includes(qk.src) ? (
                                 (() => {
-                                    const WinApp =
-                                        fi[qk.src];
+                                    const WinApp = fi[qk.src];
                                     return <WinApp />;
                                 })()
-                            ) : Object.keys(fa).includes(
-                                qk.src
-                            ) ? (
+                            ) : Object.keys(fa).includes(qk.src) ? (
                                 (() => {
-                                    const WinApp =
-                                        fa[qk.src];
+                                    const WinApp = fa[qk.src];
                                     return <WinApp />;
                                 })()
                             ) : (
@@ -560,17 +529,11 @@ function DesktopComponent({ pnstates }) {
                                     ui={qk.ui}
                                     src={qk.src}
                                     width={14}
-                                    invert={
-                                        pnstates[idx]
-                                            ? true
-                                            : null
-                                    }
+                                    invert={pnstates[idx] ? true : null}
                                 />
                             )}
                         </div>
-                        <div className="qktext">
-                            {t[qk.name]}
-                        </div>
+                        <div className="qktext">{t[qk.name]}</div>
                     </div>
                 ))}
             </div>
@@ -578,20 +541,13 @@ function DesktopComponent({ pnstates }) {
                 <hr className="mb-4" />
                 <div className="listBtn">
                     {sidepane.shortcuts.map((qk, idx) => (
-                        <div
-                            key={idx}
-                            className="qkGrp t"
-                        >
+                        <div key={idx} className="qkGrp t">
                             <div
                                 style={{
                                     fontSize: '0.8rem'
                                 }}
                                 className="qkbtn handcr prtclk"
-                                onClick={() =>
-                                    Actions.clickShortCut(
-                                        qk.val
-                                    )
-                                }
+                                onClick={() => Actions.clickShortCut(qk.val)}
                             >
                                 {qk.name}
                             </div>
