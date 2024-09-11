c7248b5e4fe45be9e4fbfceca710d2fdca338d5f
carosaome <sieunhankiet@gmail.com>
2024-09-11 09:41:27 +0700

    disable payment

--- src/containers/applications/apps/payment.jsx
+++ src/containers/applications/apps/payment.jsx
@@ -126,17 +126,31 @@ export const PaymentApp = () => {
             return;
         }
         if (isRejectHourSub(sub.name)) {
+
+            if (sub.name != 'hour_02') {
+                appDispatch(
+                    popup_open({
+                        type: 'complete',
+                        data: {
+                            success: false,
+                            content:
+                                'Gói hiện tại đang đóng do không đủ hạ tầng để phục vụ! Thời gian dự kiến: 11/11 - 25/11'
+                        }
+                    })
+                );
+                return;
+            }
+
             appDispatch(
                 popup_open({
                     type: 'complete',
                     data: {
                         success: false,
-                        content:
-                            'Gói giờ hiện tại đang đóng!. Xin vui lòng chọn gói khác'
+                        content: 'Gói giờ đang đóng do không đủ hạ tầng để phục vụ! Thời gian dự kiến: 11/11 - 7/12'
                     }
                 })
             );
-            return;
+            return
         }
         //payment(
         //    sub.price_in_vnd
@@ -172,16 +186,26 @@ export const PaymentApp = () => {
         );
     };
 
+    const isOldUser = () => {
+
+        let check = false
+        check = user?.stat?.plan_name == 'hour_02' &&
+            user?.stat?.plan_name == 'month_01' &&
+            user?.stat?.plan_name == 'unlimited_01';
+        return check
+    }
+
     const isRejectHourSub = (subName) => {
-        let check = false;
-        check =
-            subName == 'hour_02' &&
-            !isAvailableHourSub &&
-            user?.stat?.plan_name !== 'hour_02';
-        //&&
-        //user?.stat?.plan_name !== 'month_01' &&
-        //user?.stat?.plan_name !== 'unlimited_01';
-        return check;
+        //let check = false;
+        //check =
+        //    subName == 'hour_02' &&
+        //    !isAvailableHourSub &&
+        //    user?.stat?.plan_name !== 'hour_02';
+
+        //return check;
+        let check = false
+        check = !user?.stat?.plan_name || subName == 'hour_02'
+        return check
     };
     return (
         <div
@@ -325,7 +349,7 @@ export const PaymentApp = () => {
 
                                             <div className="flex flex-col gap-2 mt-auto prose">
                                                 {sub.name == 'hour_02' &&
-                                                !isRejectHourSub(sub.name) ? (
+                                                    !isRejectHourSub(sub.name) ? (
                                                     <div className="flex gap-3 items-center ">
                                                         <b>Số giờ mua</b>
                                                         <input
@@ -365,13 +389,12 @@ export const PaymentApp = () => {
                                                             shadow-sm w-full flex items-center 
                                                             justify-center text-sm 
                                                             leading-4 px-3 py-2
-                                                            ${
-                                                                isRejectHourSub(
-                                                                    sub.name
-                                                                )
-                                                                    ? 'bg-red-500'
-                                                                    : 'bg-[#328cff]'
-                                                            }  `}
+                                                            ${isRejectHourSub(
+                                                        sub.name
+                                                    )
+                                                            ? 'bg-red-500'
+                                                            : 'bg-[#328cff]'
+                                                        }  `}
                                                 >
                                                     <span className="truncate font-medium text-xl">
                                                         {isRejectHourSub(
@@ -448,10 +471,10 @@ const Payment = ({ onClose, price, subInfo, iframe = '' }) => {
             e.key == 'Enter'
                 ? nextPage()
                 : e.key == 'ArrowLeft'
-                  ? prevPage()
-                  : e.key == 'ArrowRight'
-                    ? nextPage()
-                    : null;
+                    ? prevPage()
+                    : e.key == 'ArrowRight'
+                        ? nextPage()
+                        : null;
         window.addEventListener('keydown', handle);
         return () => {
             window.removeEventListener('keydown', handle);
