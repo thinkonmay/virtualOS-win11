1d9f23c7d5e78a2099cb1b815a705c9e15df5637
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-09-25 16:35:09 +0000

    remove status handle

--- src/containers/remote/index.jsx
+++ src/containers/remote/index.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from 'react';
-import { MdArrowBackIos, MdArrowForwardIos } from 'react-icons/md';
 import { RemoteDesktopClient } from '../../../src-tauri/core/app';
 import { AudioWrapper } from '../../../src-tauri/core/pipeline/sink/audio/wrapper';
 import { VideoWrapper } from '../../../src-tauri/core/pipeline/sink/video/wrapper';
@@ -10,7 +9,6 @@ import {
 import { afterMath } from '../../backend/actions';
 import {
     appDispatch,
-    hard_reset_async,
     set_fullscreen,
     useAppSelector
 } from '../../backend/reducers';
@@ -31,7 +29,6 @@ export const Remote = () => {
     // ConnectStatus = 'not started' | 'started' | 'connecting' | 'connected' | 'closed'
     const [videoConnectivity, setVideoConnectivity] = useState('not started');
     const [audioConnectivity, setAudioConnectivity] = useState('not started');
-    const [isOpenStats, setOpenStats] = useState(true);
     const remote = useAppSelector((store) => store.remote);
     const remoteVideo = useRef(null);
     const remoteAudio = useRef(null);
@@ -58,35 +55,11 @@ export const Remote = () => {
                 setAudioConnectivity('started');
                 setVideoConnectivity('started');
             }
-
-            //Log(LogLevel.Infor,`${message} ${text ?? ""} ${source ?? ""}`)
         });
 
         SetupWebRTC();
     }, [remote.active]);
 
-    //useEffect(() => {
-    //    const got_stuck_one = () => {
-    //        return ((['started', 'closed'].includes(videoConnectivity) && audioConnectivity == 'connected') ||
-    //            (['started', 'closed'].includes(audioConnectivity) && videoConnectivity == 'connected'))
-    //    }
-    //    const got_stuck_both = () => {
-    //        return (['started', 'closed'].includes(videoConnectivity) &&
-    //            ['started', 'closed'].includes(audioConnectivity))
-    //    }
-
-    //    const check_connection = () => {
-    //        if (got_stuck_one() || got_stuck_both())
-    //            SetupWebRTC()
-    //    }
-
-    //    if (got_stuck_one() || got_stuck_both()) {
-    //        console.log('stuck condition happended, retry after 5s')
-    //        const interval = setTimeout(check_connection, 7 * 1000)
-    //        return () => { clearTimeout(interval) }
-    //    }
-
-    //}, [videoConnectivity, audioConnectivity])
     useEffect(() => {
         if (client == null) return;
         else if (isMobile()) client?.PointerVisible(true);
@@ -122,36 +95,6 @@ export const Remote = () => {
         );
     };
 
-    const HandleSuggestion = () => {
-        let elem = '';
-        if (videoConnectivity == 'closed' && audioConnectivity == 'closed') {
-            return (
-                <div className="mt-4 flex gap-2 items-center">
-                    <button
-                        onClick={() => appDispatch(hard_reset_async())}
-                        className="instbtn"
-                    >
-                        Reset
-                    </button>
-                    hoặc bật lại máy nếu <b>5'</b> chưa có hình & tiếng
-                </div>
-            );
-        }
-        if (videoConnectivity == 'closed' || audioConnectivity == 'closed') {
-            return (
-                <div className="mt-4 flex gap-2 items-center">
-                    <button
-                        onClick={() => appDispatch(hard_reset_async())}
-                        className="instbtn"
-                    >
-                        Reset
-                    </button>
-                    nếu sau <b>3'</b> chưa có hình hoặc tiếng
-                </div>
-            );
-        }
-    };
-
     return (
         <div className="relative">
             <video
@@ -164,7 +107,6 @@ export const Remote = () => {
                               afterMath(e);
                           }
                 }
-                //style={{ backgroundImage: `url(img/wallpaper/${wall.src})` }}
                 autoPlay
                 muted
                 playsInline
@@ -179,29 +121,6 @@ export const Remote = () => {
                 loop={true}
                 style={{ zIndex: -5, opacity: 0 }}
             ></audio>
-
-            <div
-                className={`${
-                    isOpenStats ? 'slide-in' : 'slide-out'
-                } bg-red-300   statusConnection`}
-            >
-                <p>
-                    Video status: <b>{videoConnectivity}</b>
-                    <br />
-                    Audio status: <b>{audioConnectivity}</b>
-                </p>
-                <button
-                    className="btn-show"
-                    onClick={() => setOpenStats((old) => !old)}
-                >
-                    {isOpenStats ? (
-                        <MdArrowBackIos style={{ fontSize: '1.2rem' }} />
-                    ) : (
-                        <MdArrowForwardIos style={{ fontSize: '1.2rem' }} />
-                    )}
-                </button>
-                <HandleSuggestion />
-            </div>
         </div>
     );
 };
