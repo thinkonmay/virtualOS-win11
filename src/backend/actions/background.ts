5a6c7098b001e598abf34a1841bec489177fbd36
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-10-07 08:05:02 +0000

    redirect to correct domain

--- src/backend/actions/background.ts
+++ src/backend/actions/background.ts
@@ -112,9 +112,18 @@ const startAnalytics = async () => {
 };
 
 const fetchSubscription = async () => {
+    const allowed_domains = 'thinkmay.net';
     await appDispatch(fetch_subscription());
 
-    const { plan, status } = store.getState().user.subscription as any;
+    const { plan, status, cluster } = store.getState().user.subscription as any;
+    if (
+        cluster != undefined &&
+        !window.location.host.includes('localhost') &&
+        cluster != window.location.host &&
+        cluster.includes(allowed_domains)
+    )
+        window.open(`https://${cluster}`, '_self');
+
     let app: string = undefined;
     if (status == 'PENDING') app = 'payment';
     else if (status == 'PAID' && (plan as string).includes('month'))
