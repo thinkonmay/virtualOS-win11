57e1f3f9b521b9f46184cc912bc83d2a314ea65f
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-10-16 05:25:13 +0000

    fix url

--- src/backend/utils/constant.ts
+++ src/backend/utils/constant.ts
@@ -1,5 +1,6 @@
 export const externalLink = {
-    GUIDELINE_DESKTOP_LINK: 'https://youtu.be/asrq0ArmCY4?si=KMUvnWdjDGXOrt2n',
+    GUIDELINE_DESKTOP_LINK:
+        'https://www.youtube.com/embed/asrq0ArmCY4?si=9SdJDxoYBCDLrQaw',
     DISCORD_LINK: 'https://discord.gg/96BesGqXQk',
     FACEBOOK_LINK: 'https://www.facebook.com/thinkonmay'
 };
