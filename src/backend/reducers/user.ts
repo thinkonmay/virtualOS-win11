964e65e9433d1e93f7b202064b16d16691bbbdfa
epitchi <thienvanlea1@gmail.com>
2024-10-23 22:46:08 +0700

    fix interval day

--- src/backend/reducers/user.ts
+++ src/backend/reducers/user.ts
@@ -334,11 +334,11 @@ export const userAsync = {
             const {
                 data: [_plans],
                 error: errrr
-            } = await GLOBAL().from('plans').select('id').eq('name', plan_name);
+            } = await GLOBAL().from('plans').select('id, policy->>total_days').eq('name', plan_name);
             if (errrr) throw new Error(errrr.message);
             else if (_plans == undefined)
                 throw new Error('gói dịch vụ hiện đang tạm đóng');
-            const { id: plan } = _plans;
+            const { id: plan, total_days } = _plans;
 
             const {
                 data: [cluster_ele],
@@ -364,7 +364,7 @@ export const userAsync = {
                     plan,
                     cluster,
                     local_metadata: {},
-                    ended_at: addDays(new Date(), 30).toISOString()
+                    ended_at: addDays(new Date(), Number.parseInt(total_days)).toISOString()
                 })
                 .select('id');
             if (error) throw new Error(error.message);
