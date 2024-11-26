ee88e6ce07a7209e5553df0be6fa404d21ad5b0f
carosaome <sieunhankiet@gmail.com>
2024-11-26 11:47:47 +0700

    change log

--- src/containers/popup/modal/extend.jsx
+++ src/containers/popup/modal/extend.jsx
@@ -17,43 +17,43 @@ export function extendService({ data: { type, to, available_time } }) {
     return (
         <div className="w-[320px] h-auto p-[14px] rounded-lg flex flex-col gap-y-5">
             <div className="flex justify-center items-center gap-2 text-[#B0D0EF]">
-                <MdInfoOutline className="text-3xl"></MdInfoOutline>
-                {type == 'hour_limit' && available_time != undefined ? (
-                    <h3 className="text-xl">
-                        Dịch vụ hết hạn ngày <br /> {to}
-                        <br />
-                        Dịch vụ còn lại <br /> {available_time.toFixed(1)}h
-                        <br />
-                    </h3>
-                ) : (
-                    <h3 className="text-xl">
-                        Dịch vụ hết hạn ngày <br /> {to}
-                    </h3>
-                )}
+                <MdInfoOutline className="text-4xl"></MdInfoOutline>
+                <h3>Dịch vụ sắp hết hạn</h3>
             </div>
 
             <div>
                 <p className="mt-[8px]">
-                    Dữ liệu của bạn sẽ được dọn dẹp sau khi dịch vụ hết hạn
+                    {type == 'hour_limit' && available_time != undefined ? (
+                        <>
+                            <b>Thời gian:</b> {to}
+                            <br />
+                            <b>Số giờ còn lại:</b> {available_time.toFixed(1)}h
+                            <br />
+                        </>
+                    ) : (
+                        <>
+                            Thời gian: <br /> {to}
+                        </>
+                    )}
                     <br></br>
+                    <b>Lưu ý:</b> Dữ liệu sẽ bị xoá sau khi hết hạn
                     <br></br>
-                    Gia hạn ngay?
                 </p>
             </div>
-            <div className="flex gap-3 justify-center mt-3 mb-2">
+            <div className="flex gap-3 justify-end mt-3 mb-2">
                 <button
                     style={{ padding: '6px 14px' }}
                     className="text-base font-medium rounded-md"
                     onClick={deny}
                 >
-                    Không
+                    Đóng
                 </button>
                 <button
                     style={{ padding: '6px 14px' }}
                     onClick={() => appDispatch(app_toggle('payment'))}
                     className="cursor-pointer justify-center  text-base font-medium instbtn rounded-md"
                 >
-                    Có, gia hạn ngay
+                    Gia hạn ngay
                 </button>
             </div>
         </div>
