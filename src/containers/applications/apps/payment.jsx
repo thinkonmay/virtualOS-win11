3f862330b08d5215c9e45cba32342ff749184336
carosaome <sieunhankiet@gmail.com>
2024-12-13 18:17:31 +0700

    close weekly

--- src/containers/applications/apps/payment.jsx
+++ src/containers/applications/apps/payment.jsx
@@ -18,7 +18,7 @@ import './assets/store.scss';
 
 const listSubs = [
     {
-        active: true,
+        active: false,
         highlight: false,
         title: 'Gói tuần',
         price_in_vnd: '99',
