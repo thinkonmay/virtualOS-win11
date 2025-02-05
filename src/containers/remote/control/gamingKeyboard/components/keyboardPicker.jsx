5d1cf5b600458dfc128954e1c33587a6e2f4f2ee
carosaome <sieunhankiet@gmail.com>
2025-02-05 18:50:22 +0700

    fix keyboard

--- src/containers/remote/control/gamingKeyboard/components/keyboardPicker.jsx
+++ src/containers/remote/control/gamingKeyboard/components/keyboardPicker.jsx
@@ -33,7 +33,7 @@ export const KeyboardPicker = () => {
         );
     };
 
-    const handleKeyReleased = (button) => {};
+    const handleKeyReleased = (button) => { };
     return (
         <div
             id="keyboard"
@@ -56,7 +56,7 @@ export const KeyboardPicker = () => {
                 }}
                 layout={{
                     default: [
-                        'Esc F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12',
+                        'Escape F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12',
                         '` 1 2 3 4 5 6 7 8 9 0 - = Backspace',
                         'Tab Q W E R T Y U I O P { } | ',
                         'Capslock A S D F G H J K L : " Enter',
