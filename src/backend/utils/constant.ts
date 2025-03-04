1cf8d53318a65a31a67cabbfaa097ecff330926a
carosaome <sieunhankiet@gmail.com>
2025-03-04 17:43:16 +0700

    update constant

--- src/backend/utils/constant.ts
+++ src/backend/utils/constant.ts
@@ -34,7 +34,15 @@ export const localStorageKey = {
     gamingKeyboardData: 'GAMING_KEYBOARD_DATA'
 };
 
-export type PlanName = 'month1' | 'month2' | 'week1' | 'week2' | 'ramcpu20' | '50GB' | '100GB' | '200GB';
+export type PlanName =
+    | 'month1'
+    | 'month2'
+    | 'week1'
+    | 'week2'
+    | 'ramcpu20'
+    | '50GB'
+    | '100GB'
+    | '200GB';
 
 export const pathNames = {
     VERIFY_PAYMENT: 'success'
