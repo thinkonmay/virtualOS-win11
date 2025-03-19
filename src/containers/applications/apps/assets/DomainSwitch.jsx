808d42774dad0f06b4188e51fc7fbb33c0fab17e
pigeatgarlic <huyhoangdo0205@gmail.com>
2025-03-19 03:22:36 +0000

    better domain switch

--- src/containers/applications/apps/assets/DomainSwitch.jsx
+++ src/containers/applications/apps/assets/DomainSwitch.jsx
@@ -1,11 +1,19 @@
+import { preload } from '../../../../backend/actions/background';
 import { useAppSelector } from '../../../../backend/reducers';
 
 function DomainSwitch() {
     const domain = useAppSelector((state) => state.worker.currentAddress);
 
     const updateLanguage = (e) => {
-        localStorage.setItem('thinkmay_domain', e.target.value);
-        window.location.reload();
+        const domain = e.target.value;
+        const listDomain = ['play.thinkmay.net', 'play.3.thinkmay.net'];
+        const v2_domain = ['play.2.thinkmay.net', 'v4.thinkmay.net'];
+        if (listDomain.includes(domain))
+            window.open(`https://${domain}`, '_self');
+        else if (v2_domain.includes(domain)) {
+            localStorage.setItem('thinkmay_domain', domain);
+            preload(false);
+        }
     };
 
     return (
