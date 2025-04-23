390d63c1dd18af77693c5e4238ad422c3bffac28
pigeatgarlic <huyhoangdo0205@gmail.com>
2025-04-23 12:55:39 +0000

    fix share btn

--- src/containers/popup/modal/share.jsx
+++ src/containers/popup/modal/share.jsx
@@ -17,7 +17,7 @@ export function share({ data: { ref, discount_code } }) {
         const url = ref != undefined ? new URL(ref) : new URL(originalurl.href);
         url.searchParams.set('ref', discount_code);
         setURL(url.toString());
-        navigator.clipboard.writeText(url);
+        navigator.clipboard.writeText(url.toString());
     }, []);
 
     const handleCopy = () => {
