4ecf549201b6100ec2c89b9349a419d897ebfb0c
pigeatgarlic <huyhoangdo0205@gmail.com>
2025-05-13 14:13:00 +0000

    Revert "temp disable sub file"
    This reverts commit 7b6e3292758bfda2684b632036cc6237fcb7bf21.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/play',
-    build: { outDir: './dist' },
+    build: { outDir: '../public/play' },
     define: {
         'import.meta.env.__BUILD__': `"${new Date().toUTCString()}"`
     },
