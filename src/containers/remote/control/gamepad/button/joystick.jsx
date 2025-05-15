cbda623bd6838afc7712db886007994f13706c37
pigeatgarlic <huyhoangdo0205@gmail.com>
2025-05-15 15:10:22 +0000

    use lower level api for joystick

--- src/containers/remote/control/gamepad/button/joystick.jsx
+++ src/containers/remote/control/gamepad/button/joystick.jsx
@@ -6,11 +6,11 @@ const knobRadius = 0.3;
 
 export const CustomJoyStick = memo(({ size = 100, isRight = true }) => {
     const id = useId();
-    const [touching, setTouching] = useState(false);
     const ref = useRef(null);
     const knobRef = useRef(null);
 
-    const updatePosition = (touch) => {
+    const updatePosition = (event) => {
+        const touch = event.changedTouches?.[0]
         const rect = ref.current.getBoundingClientRect();
         let x = (touch.clientX - rect.left - rect.width / 2) * speed;
         let y = (touch.clientY - rect.top - rect.height / 2) * speed;
@@ -30,18 +30,9 @@ export const CustomJoyStick = memo(({ size = 100, isRight = true }) => {
         knobRef.current.style.transform = `translate(${x}px, ${y}px)`;
     };
 
-    const handlePointerDown = (e) => {
-        e.target.setPointerCapture(e.pointerId);
-        setTouching(true);
-    };
-
-    const handlePointerMove = (e) => {
-        if (!touching) return;
-        updatePosition(e);
-    };
+    const handlePointerMove = (e) => updatePosition(e)
 
     const handlePointerUp = async () => {
-        setTouching(false);
         knobRef.current.style.transform = 'translate(0px, 0px)';
         gamepadAxis(0, 0, isRight);
     };
@@ -63,10 +54,9 @@ export const CustomJoyStick = memo(({ size = 100, isRight = true }) => {
                 touchAction: 'none',
                 position: 'relative'
             }}
-            onPointerDown={handlePointerDown}
-            onPointerMove={handlePointerMove}
-            onPointerUp={handlePointerUp}
-            onPointerCancel={handlePointerUp}
+            onTouchMove={handlePointerMove}
+            onTouchEnd={handlePointerUp}
+            onTouchCancel={handlePointerUp}
         >
             <div
                 ref={knobRef}
@@ -77,7 +67,7 @@ export const CustomJoyStick = memo(({ size = 100, isRight = true }) => {
                     backgroundColor: 'rgba(255, 255, 255,0.5',
                     position: 'absolute',
                     boxShadow: 'rgba(255, 255, 255, 0.1) 0px 0px 4px 5px',
-                    transition: touching ? 'none' : 'transform 0.05s ease-out'
+                    transition: 'none'
                 }}
             />
         </div>
