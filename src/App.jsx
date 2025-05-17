93dbee1795c73e89ae09fb5143c862eb93157a8c
pigeatgarlic <huyhoangdo0205@gmail.com>
2025-05-17 03:19:49 +0000

    allow edit layout from mainscreen

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import { Toaster } from 'react-hot-toast';
 import { UserEvents } from '../src-tauri/api';
 import { PreloadBackground } from './backend/actions/background';
 import { afterMath } from './backend/actions/index';
-
+import { VirtualGamepad } from './containers/remote/control/gamepad';
+import GamingKeyboard from './containers/remote/control/gamingKeyboard';
+import { Plugin } from './containers/remote/control/plugin';
+import { VirtKeyboard } from './containers/remote/control/keyboard';
 import {
     appDispatch,
     menu_show,
@@ -25,6 +28,7 @@ import { Remote } from './containers/remote';
 import { Status } from './containers/status';
 import { ErrorFallback } from './error';
 import './index.css';
+import { isMobile } from '../src-tauri/core';
 
 function App() {
     document.body.dataset.theme = 'dark';
@@ -35,6 +39,18 @@ function App() {
     const pointerLock = useAppSelector((state) => state.remote.pointer_lock);
     const [booting, setLockscreen] = useState(true);
     const [loadingText, setloadingText] = useState(Contents.BOOTING);
+
+    const keyboard = useAppSelector(
+        (state) => !state.sidepane.mobileControl.keyboardHide
+    );
+    const gamingKeyboard = useAppSelector(
+        (state) => state.sidepane.mobileControl.gamingKeyBoard.open
+    );
+    const gamepad = useAppSelector(
+        (state) =>
+            !state.sidepane.mobileControl.gamePadHide ||
+            state.sidepane.mobileControl.gamepadSetting.draggable
+    );
 
     const ctxmenu = (e) => {
         afterMath(e);
@@ -166,7 +182,10 @@ function App() {
                 <div className="appwrap ">
                     {pointerLock ? null : (
                         <>
-                            <Taskbar />
+                            {(keyboard || gamepad || gamingKeyboard) &&
+                            !remote.active ? null : (
+                                <Taskbar />
+                            )}
                             <ActMenu />
                             <StartMenu />
                             <SidePane />
@@ -181,8 +200,20 @@ function App() {
                         </>
                     )}
                     {remote.active && !pointerLock ? <Status /> : null}
+                    {isMobile() ? (
+                        keyboard ? (
+                            <VirtKeyboard />
+                        ) : gamepad ? (
+                            <VirtualGamepad />
+                        ) : gamingKeyboard ? (
+                            <GamingKeyboard />
+                        ) : null
+                    ) : null}
                     {remote.active && loggedIn ? (
-                        <Remote />
+                        <>
+                            <Plugin />
+                            <Remote />
+                        </>
                     ) : (
                         <>
                             <Background />
--- src/containers/remote/index.jsx
+++ src/containers/remote/index.jsx
@@ -1,47 +1,22 @@
 import { useEffect, useRef } from 'react';
-import { LuClipboardCopy, LuClipboardPaste } from 'react-icons/lu';
-import {
-    MdGamepad,
-    MdOutlineKeyboard,
-    MdOutlineSportsEsports
-} from 'react-icons/md';
 import {
     AudioWrapper,
     RemoteDesktopClient,
     VideoWrapper,
     isMobile
 } from '../../../src-tauri/core';
-import { Assign, ready, virtMouseWheel } from '../../../src-tauri/singleton';
-import { clickShortCut, showConnect } from '../../backend/actions';
+import { Assign, ready } from '../../../src-tauri/singleton';
+import { showConnect } from '../../backend/actions';
 import {
     appDispatch,
-    open_gaming_keyboard,
     popup_close,
     set_fullscreen,
-    toggle_gamepad,
-    toggle_keyboard,
     toggle_objectfit,
     useAppSelector
 } from '../../backend/reducers';
-import { Icon } from '../../components/shared/general';
-import { VirtualGamepad } from './control/gamepad';
-import GamingKeyboard from './control/gamingKeyboard';
-import { VirtKeyboard } from './control/keyboard';
 import './remote.scss';
 
 export const Remote = () => {
-    const keyboard = useAppSelector(
-        (state) => !state.sidepane.mobileControl.keyboardHide
-    );
-    const gamingKeyboard = useAppSelector(
-        (state) => state.sidepane.mobileControl.gamingKeyBoard.open
-    );
-    const gamepad = useAppSelector(
-        (state) => !state.sidepane.mobileControl.gamePadHide
-    );
-    const draggable = useAppSelector(
-        (state) => state.sidepane.mobileControl.gamepadSetting.draggable
-    );
     const { active, auth, relative_mouse, fullscreen, objectFit } =
         useAppSelector((store) => store.remote);
     const remoteVideo = useRef(null);
@@ -79,18 +54,6 @@ export const Remote = () => {
     };
     return (
         <div className="relative">
-            {isMobile() ? (
-                keyboard ? (
-                    <VirtKeyboard />
-                ) : gamepad || draggable ? (
-                    <VirtualGamepad />
-                ) : gamingKeyboard ? (
-                    <GamingKeyboard />
-                ) : (
-                    <Plugin />
-                )
-            ) : null}
-
             <video
                 className="remote"
                 ref={remoteVideo}
@@ -113,79 +76,5 @@ export const Remote = () => {
                 style={{ zIndex: -5, opacity: 0 }}
             ></audio>
         </div>
-    );
-};
-
-const Plugin = () => {
-    return (
-        <>
-            <div
-                onClick={() => appDispatch(open_gaming_keyboard())}
-                className="z-10 absolute bottom-16 right-4 flex items-center justify-center rounded-sm bg-[#212121c4] w-[32px] h-[24px] text-[#ffffffe6]"
-            >
-                <MdGamepad fontSize={'1.4rem'} />
-            </div>
-            <div
-                onClick={() => appDispatch(toggle_keyboard())}
-                className="z-10 absolute bottom-5 right-4 flex items-center justify-center rounded-sm bg-[#212121c4] w-[32px] h-[24px] text-[#ffffffe6]"
-            >
-                <MdOutlineKeyboard fontSize={'1.4rem'} />
-            </div>
-            <div
-                onClick={() => appDispatch(toggle_gamepad())}
-                className="z-10 absolute bottom-5 left-4 flex items-center justify-center rounded-sm bg-[#212121c4] w-[32px] h-[24px] text-[#ffffffe6]"
-            >
-                <MdOutlineSportsEsports fontSize={'1.4rem'} />
-            </div>
-
-            <div className="z-10 absolute bottom-[40%]  left-4 flex flex-col gap-4">
-                <button
-                    className="py-2 px-1 rounded-md bg-[#212121c4]"
-                    onClick={() => clickShortCut(['control', 'v'])}
-                >
-                    <LuClipboardPaste width={32} color="#fff" />
-                </button>
-
-                <button
-                    className="py-2 px-1 rounded-md bg-[#212121c4]"
-                    onClick={() => clickShortCut(['control', 'c'])}
-                >
-                    <LuClipboardCopy width={32} color="#fff" />
-                </button>
-            </div>
-
-            <div className="z-10 absolute bottom-[40%]  right-4 flex flex-col gap-4">
-                <button
-                    className="py-2 rounded-md bg-[#212121c4]"
-                    onClick={() => virtMouseWheel(-150)}
-                >
-                    <Icon src="mouseUp" width={32} />
-                </button>
-
-                <button
-                    className="py-2 rounded-md bg-[#212121c4]"
-                    onClick={() => virtMouseWheel(150)}
-                >
-                    <Icon src="mouseDown" width={32} />
-                </button>
-            </div>
-
-            <div className="z-10 absolute top-16 left-4 flex flex-col gap-4">
-                <button
-                    className="py-2 px-1 rounded-md bg-[#212121c4] text-white"
-                    onClick={() => clickShortCut(['lwin', 'd'])}
-                >
-                    home
-                </button>
-            </div>
-            <div className="z-10 absolute top-16 right-4 flex flex-col gap-4">
-                <button
-                    className="py-2 px-1 rounded-md bg-[#212121c4] text-white"
-                    onClick={() => clickShortCut(['Enter'])}
-                >
-                    enter
-                </button>
-            </div>
-        </>
     );
 };
