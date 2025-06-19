417db72062ef2e0e41a1793c40bcb61cfe47fc9d
epitchi <thienvanlea1@gmail.com>
2025-06-20 00:48:10 +0700

    change @thinkmay.net to @gmail.com for mailing

--- src/backend/actions/index.ts
+++ src/backend/actions/index.ts
@@ -183,6 +183,10 @@ export const signUpWithEmail = async (
     password: string,
     passwordConfirm: string
 ) => {
+
+    if (!email.includes('@gmail.com'))
+        throw new Error("email must have @gmail.com")
+
     return POCKETBASE()
         .collection('users')
         .create({
--- src/containers/popup/modal/login.jsx
+++ src/containers/popup/modal/login.jsx
@@ -119,7 +119,7 @@ export function login({ loading }) {
                             name="email"
                             id="email"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                            placeholder="name@thinkmay.net"
+                            placeholder="name@gmail.com"
                             required
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
